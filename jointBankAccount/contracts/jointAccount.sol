52c707bb49315aa6df5b24f08b479c36a470d2df
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-03-04 15:17:49 +0530

    contract

--- jointBankAccount/contracts/jointAccount.sol
+++ jointBankAccount/contracts/jointAccount.sol
@@ -4,6 +4,25 @@ pragma solidity 0.8.19;
 
 contract BankAccount {
     event Deposit (address indexed user, uint256 indexed accountId, uint256 value, uint256 timestamp);
-    event withDrawRequest (address indexed user, uint256 indexed accountId, uint256 indexed withdrawId, uint256 amount, uint256 timeStamp);
+    event withdrawRequested (address indexed user, uint256 indexed accountId, uint256 indexed withdrawId, uint256 amount, uint256 timeStamp);
+    event Withdraw (uint indexed withdrwaId, uint timestamp);
+    event AccountCreated (address[] owners, uint indexed id, uint timpstamp);
+
+    struct withdrawRequest {
+        address users;
+        uint amount;
+        uint approvals;
+        mapping(address => bool) ownersApproved;
+        bool approved;
+    }
+
+    struct Account {
+        address[] owners;
+        uint balance;
+        mapping(uint => withdrawRequest) withdraweRequests;
+    }
+
+    mapping(uint => Account) accounts;
+    mapping(address => uint[]) userAccounts;
 
 }
