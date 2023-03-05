6bed50ac0a7c7516e2205664776a4f73704aa2cb
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-03-05 19:46:07 +0530

    Tests

--- jointBankAccount/test/Lock.js
+++ jointBankAccount/test/Lock.js
@@ -3,12 +3,25 @@ const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
 describe("BankAccount", function () {
-  async function deployOneYearLockFixture() {
-    const [owner, otherAccount] = await ethers.getSigners();
+  async function deployBankAccount() {
+    const [addr0, addr1, addr2, addr3, addr4] = await ethers.getSigners();
 
     const BankAccount = await ethers.getContractFactory("BankAccount");
     const bankAccount = await BankAccount.deploy();
 
-    return { bankAccount, owner, otherAccount };
+    return { bankAccount, addr0, addr1, addr2, addr3, addr4 };
   }
+
+  describe("Deployment", () => {
+    it("Should deploy without error", async () => {
+      await loadFixture(deployBankAccount);
+    });
+  });
+
+  describe("Creating an account", () => {
+    it("Should allow creating a single user account", async () => {
+      const { bankAccount, addr0 } = await loadFixture(deployBankAccount);
+      await bankAccount.connect(addr0).createAccount([]);
+    });
+  });
 });
